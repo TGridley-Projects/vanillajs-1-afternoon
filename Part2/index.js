efe8c701177d6989850a6c08359bacbdbd23f229
Trevor <alutiiq@comcast.net>
2020-07-14 22:30:03 -0700

    finished basic card color

---
+++ Part2/index.js
@@ -0,0 +1,13 @@
+console.log('The house always wins!');
+
+let idInput = document.getElementById('idInput');
+let colorInput = document.getElementById('colorInput');
+
+console.log(idInput);
+console.log(colorInput);
+
+function setCard(){
+    const card = document.getElementById(idInput.value);
+    card.style.color = colorInput.value
+    console.log(card);
+}
